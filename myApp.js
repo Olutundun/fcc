fa1b0237bd16e2f491c01dfef43a88d6e85bfe2e
Your Name <tundun.deborah@gmail.com>
2022-01-22 19:08:25 -0500

    "testing console"

--- myApp.js
+++ myApp.js
@@ -1,1 +1,39 @@
-console.log("Hello World");
+var express = require('express');
+var app = express();
+
+console.log("Hello World")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
